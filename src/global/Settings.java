e24f233ea901b65e2e6fbb506f0c57ebeb6c0e98
billb <billb@192.168.1.133>
2017-01-04 07:14:16 -0500

    Implemented sequential settings

--- src/global/Settings.java
+++ src/global/Settings.java
@@ -253,5 +253,20 @@ public class Settings {
 
 	public static void reset(int i) {
 		
+		G.blocksWide = blocksWide.get(i % blocksWide.size());
+		i /= blocksWide.size();
+		G.triangles = maxTriangles.get(i % maxTriangles.size());
+		i /= maxTriangles.size();
+		G.samples = samples.get(i % samples.size());
+		i /= samples.size();
+		G.randomBlockMult = randomBlocks.get(i % randomBlocks.size());
+		i /= randomBlocks.size();
+		G.scale = scales.get(i % scales.size());
+		i /= scales.size();
+		G.postScale = postScales.get(i % postScales.size());
+		i /= postScales.size();
+		G.finalScale = finalScales.get(i % finalScales.size());
+		//System.out.println(G.getShortTitle());
+		
 	}
 }
